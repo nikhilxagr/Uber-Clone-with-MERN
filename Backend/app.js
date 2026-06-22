e99dcbae84cf5b877ffbe10ae62e338d256ccf48
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-06-22 23:29:23 +0530

    added ride creation models  and services and controllers

--- Backend/app.js
+++ Backend/app.js
@@ -1,39 +1,29 @@
-const express = require('express');
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const cors = require('cors');
-const userRoutes = require('./routes/user.routes');
-const cookieParser = require('cookie-parser');  
-const captainRoutes = require('./routes/captain.routes');
+const cookieParser = require("cookie-parser");
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 const mapsRoutes = require("./routes/maps.routes");
+const rideRoutes = require("./routes/ride.routes");
+
+connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api/users', userRoutes);
-app.use('/users', userRoutes);
-app.use('/api/captain', captainRoutes);
-app.use('/api/captains', captainRoutes);
-app.use('/captain', captainRoutes);
-app.use('/captains', captainRoutes);
-app.use('/api/maps', mapsRoutes);
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-}); 
-
-app.use((err, req, res, next) => {
-    console.error(err);
-
-    if (res.headersSent) {
-        return next(err);
-    }
-
-    res.status(err.status || 500).json({
-        error: err.message || 'Internal server error'
-    });
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
+app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
+app.use("/maps", mapsRoutes);
+app.use("/rides", rideRoutes);
 
 module.exports = app;
