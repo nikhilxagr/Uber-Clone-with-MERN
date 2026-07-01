ca38b8557aa054d83710e06da82111193738f1c9
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-07-01 23:46:09 +0530

    Add ride socket notifications
    Wire up Socket.IO on both client and server to join users, track captain location, and push new ride events to nearby captains. Also updates captain schema/location fields and adds profile-based user protection in the frontend.

--- Backend/services/maps.service.js
+++ Backend/services/maps.service.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const captainModel = require("../models/captain.model");
 
 module.exports.getAddressCoordinate = async (address) => {
   const apiKey = process.env.GOOGLE_MAPS_API;
@@ -68,4 +69,18 @@ module.exports.getAutoCompleteSuggestions = async (input) => {
     console.error(err);
     throw err;
   }
+};
+
+module.exports.getCaptainsInTheRadius = async (ltd, lng, radius) => {
+  // radius in km
+
+  const captains = await captainModel.find({
+    location: {
+      $geoWithin: {
+        $centerSphere: [[ltd, lng], radius / 6371],
+      },
+    },
+  });
+
+  return captains;
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,17 +5,16 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from "./context/UserContext.jsx";
 import CaptainContext from "./context/CapatainContext.jsx";
-import { CaptainLogout } from "./pages/CaptainLogout.jsx";
-import { UserLogout } from "./pages/UserLogout.jsx";
+import SocketProvider from "./context/SocketContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <CaptainContext>
-      <UserContext>
+  <CaptainContext>
+    <UserContext>
+      <SocketProvider>
         <BrowserRouter>
           <App />
         </BrowserRouter>
-      </UserContext>
-    </CaptainContext>
-  </StrictMode>,
+      </SocketProvider>
+    </UserContext>
+  </CaptainContext>,
 );
